504c995879048b3acea1d196353f6ba77ccd5bda
Nitin Bhargava <nitinbhargava@Nitins-MacBook-Pro.local>
2019-04-18 00:21:17 -0700

    new website added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myWebsite';
+  a = [1,2,3,4];
+  b = [22,33,1,45,6];
+
+  addMore(){
+    this.a = this.a.concat(this.b);
+  }
+  reset(){
+    this.a = [1,2,3,4];
+  }
 }
